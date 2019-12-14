1ca7e0eb470e1aafc9ca4c812bc0fe1871dd800d
mrchenfm <1499967901@qq.com>
2019-12-14 13:55:39 +0800

    修改项目名称

---
+++ cnr_framework_common/src/main/java/com/ecut/cnr/framework/common/utils/IdUtils.java
@@ -0,0 +1,11 @@
+package com.ecut.cnr.framework.common.utils;
+
+/**
+ * @Classname IdUtils
+ * @Description id生成工具
+ * @Date 2019/12/14 10:22
+ * @Create by fangming_chen
+ */
+public class IdUtils {
+
+}
